3a7ff1e4979afebcad732cf5a2ae7099f6833102
Ayoub Senhaji <ayoub.senhaji@mazars.fr>
2024-10-17 20:31:17 +0100

    first commit, has most logic done, except for multiplayer. oh yeah and it has no design whatsoever

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,105 @@
-import Image from "next/image";
+"use client";
+import { useState, useCallback, useEffect } from "react";
+import dictionaryJson from "../assets/words_dictionary.json";
 
+const dictionary = dictionaryJson as unknown as Dictionary;
+
+interface Dictionary {
+  [word: string]: boolean;
+}
+
+interface Styles {
+  [key: string]: string;
+}
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [currentword, setCurrentWord] = useState<string>("");
+  const [randomString, setRandomString] = useState<string>("");
+  const [resultString, setResultString] = useState<string>("");
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+  const pickRandomStr = () => {
+    const keys = Object.keys(dictionary);
+    const randomWord = keys[Math.floor(Math.random() * keys.length)];
+
+    if (randomWord.length >= 3) {
+      const startIdx = Math.floor(Math.random() * (randomWord.length - 2));
+      const randomSubstring = randomWord.substring(startIdx, startIdx + 3);
+      setRandomString(randomSubstring);
+    } else {
+      setRandomString(randomWord);
+    }
+  };
+  useEffect(() => {
+    pickRandomStr();
+  }, []);
+
+  const submitWord = useCallback(
+    (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      if (dictionary[currentword] && currentword.includes(randomString)) {
+        setResultString("Word exists");
+        setCurrentWord("");
+        pickRandomStr();
+      } else {
+        setResultString("Dumbass");
+      }
+    },
+    [currentword, randomString]
+  );
+
+  const inputStyles: Styles = {
+    padding: "10px",
+    marginTop: "50px",
+    fontSize: "1.5rem",
+    boxShadow: "0 0 0 1px black",
+  };
+
+  const pageStyles: Styles = {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100vh",
+  };
+  return (
+    <div style={pageStyles}>
+      <h1 style={{ marginBottom: "50px" }}>{resultString}</h1>
+      <h1>{randomString}</h1>
+      <form onSubmit={submitWord}>
+        <input
+          style={inputStyles}
+          type="text"
+          value={currentword}
+          onChange={(event) => {
+            setCurrentWord(event.target.value);
+          }}
+        />
+        <input
+          style={{
+            background: "gray",
+            padding: "15px",
+            marginLeft: "20px",
+            color: "white",
+          }}
+          type="submit"
+          value="Submit"
+        />
+        <button
+          type="button"
+          style={{
+            background: "gray",
+            padding: "15px",
+            marginLeft: "20px",
+            color: "white",
+          }}
+          onClick={() => {
+            setResultString("");
+            setCurrentWord("");
+            pickRandomStr();
+          }}
         >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+          I&apos;m a dumbass
+        </button>
+      </form>
     </div>
   );
 }
