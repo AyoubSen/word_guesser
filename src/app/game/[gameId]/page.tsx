3c48312255fb55cae6ebc18f12b741e095c48b88
Ayoub Senhaji <ayoub.senhaji@mazars.fr>
2024-11-10 20:35:32 +0100

    added lives system

--- src/app/game/[gameId]/page.tsx
+++ src/app/game/[gameId]/page.tsx
@@ -23,6 +23,8 @@ export default function Home({ params }: { params: { gameId: string } }) {
   const [users, setUsers] = useState<string[]>([]);
   const [timer, setTimer] = useState<number>(10);
   const [isTimerRunning, setIsTimerRunning] = useState<boolean>(true);
+  const [gameOver, setGameOver] = useState<boolean>(false);
+  const [gameStarted, setGameStarted] = useState<boolean>(false);
 
   const timerIntervalRef = useRef<NodeJS.Timeout | null>(null);
 
@@ -41,26 +43,43 @@ export default function Home({ params }: { params: { gameId: string } }) {
     });
 
     socket.on("randomString", (sharedString: string) => {
-      setRandomString(sharedString);
-      setTimer(10);
+      if (!gameOver) {
+        setRandomString(sharedString);
+        setTimer(3);
 
-      if (timerIntervalRef.current) {
-        clearInterval(timerIntervalRef.current);
+        if (timerIntervalRef.current) {
+          clearInterval(timerIntervalRef.current);
+        }
+        timerIntervalRef.current = setInterval(() => {
+          setTimer((prev) => {
+            if (prev < 1) {
+              clearInterval(timerIntervalRef.current!);
+              timerIntervalRef.current = null;
+              socket.emit("timeOver", { roomId: id });
+              return 0;
+            }
+            return prev - 1;
+          });
+        }, 1000);
       }
-      timerIntervalRef.current = setInterval(() => {
-        setTimer((prev) => {
-          if (prev < 1) {
-            clearInterval(timerIntervalRef.current!);
-            socket.emit("timeOver", { roomId: id });
-            return 0;
-          }
-          return prev - 1;
-        });
-      }, 1000);
     });
 
     socket.on("userList", (userList: string[]) => {
       setUsers(userList);
+    });
+
+    socket.on("forbidden", (message: string) => {
+      setResultString(message);
+      setGameOver(true);
+      stopTimer();
+      setRandomString("");
+    });
+
+    socket.on("gameOver", (message: string) => {
+      setGameOver(true);
+      setResultString(message);
+      stopTimer();
+      setRandomString("");
     });
 
     return () => {
@@ -69,11 +88,21 @@ export default function Home({ params }: { params: { gameId: string } }) {
       }
       socket.disconnect();
     };
-  }, [id, userName]);
+  }, [id, userName, gameOver]);
+
+  const startGame = () => {
+    setGameStarted(true);
+    socket.emit("startGame", { roomId: id });
+  };
 
   const submitWord = useCallback(
     (e: React.FormEvent<HTMLFormElement>) => {
       e.preventDefault();
+      if (gameOver) {
+        setResultString("Game is over. You cannot continue.");
+        return;
+      }
+
       if (!currentword) {
         setResultString("Please enter a word!");
         return;
@@ -90,83 +119,128 @@ export default function Home({ params }: { params: { gameId: string } }) {
         setResultString("Try again!");
       }
     },
-    [currentword, id]
+    [currentword, id, gameOver, randomString]
   );
+
+  const stopTimer = () => {
+    if (timerIntervalRef.current) {
+      clearInterval(timerIntervalRef.current);
+      timerIntervalRef.current = null;
+    }
+    setIsTimerRunning(false);
+  };
 
   return (
     <div className="flex flex-col justify-center items-center h-screen bg-[#DFF2EB] font-sans">
       <h1 className="text-4xl mb-8 text-gray-800 font-bold">Word Game</h1>
-      <div>User count: {userCount}</div>
-      <div className="text-lg mb-4">Connected users: {users.join(", ")}</div>
-      <div className="text-5xl font-bold text-blue-500 mb-8 pb-5 p-3 rounded-lg bg-white shadow-lg">
-        {randomString}
-      </div>
-      <form
-        onSubmit={submitWord}
-        className="flex flex-col items-center w-full max-w-md"
-      >
-        <input
-          className="p-3 text-lg w-full mb-4 rounded border-2 border-gray-300 focus:outline-none"
-          type="text"
-          value={currentword}
-          onChange={(event) => setCurrentWord(event.target.value)}
-          placeholder="Enter a word containing the above letters"
-        />
-        <div className="flex justify-between w-full">
-          <button
-            type="submit"
-            className="p-3 bg-green-500 text-white font-bold rounded transition duration-300 hover:bg-green-600"
-          >
-            Submit
-          </button>
-          <button
-            type="button"
-            className="p-3 bg-red-500 text-white font-bold rounded transition duration-300 hover:bg-red-600"
-            onClick={() => {
-              setResultString("");
-              setCurrentWord("");
-            }}
-          >
-            Reset
-          </button>
-        </div>
-      </form>
-      <div className="flex flex-col gap-5">
-        <p className="mt-8 text-2xl font-bold text-gray-800">
-          Time left: {timer} seconds
-        </p>
+      {gameOver && (
+        <p className="mt-8 text-2xl font-bold text-red-600">Game Over</p>
+      )}
+      {!gameStarted ? (
+        <button
+          onClick={startGame}
+          className="p-3 bg-blue-500 text-white font-bold rounded mt-4 transition duration-300 hover:bg-blue-600"
+        >
+          Start Game
+        </button>
+      ) : (
+        <>
+          <div>User count: {userCount}</div>
+          <div className="text-lg mb-4">
+            Connected users: {users.join(", ")}
+          </div>
+          {!gameOver && (
+            <div className="text-5xl font-bold text-blue-500 mb-8 pb-5 p-3 rounded-lg bg-white shadow-lg">
+              {randomString}
+            </div>
+          )}
+          {!gameOver && (
+            <form
+              onSubmit={submitWord}
+              className="flex flex-col items-center w-full max-w-md"
+            >
+              <input
+                className="p-3 text-lg w-full mb-4 rounded border-2 border-gray-300 focus:outline-none"
+                type="text"
+                value={currentword}
+                onChange={(event) => setCurrentWord(event.target.value)}
+                placeholder="Enter a word containing the above letters"
+                disabled={gameOver}
+              />
+              <div className="flex justify-between w-full">
+                <button
+                  type="submit"
+                  className="p-3 bg-green-500 text-white font-bold rounded transition duration-300 hover:bg-green-600"
+                  disabled={gameOver}
+                >
+                  Submit
+                </button>
+                <button
+                  type="button"
+                  className="p-3 bg-red-500 text-white font-bold rounded transition duration-300 hover:bg-red-600"
+                  onClick={() => {
+                    setResultString("");
+                    setCurrentWord("");
+                  }}
+                  disabled={gameOver}
+                >
+                  Reset
+                </button>
+              </div>
+            </form>
+          )}
+          <div className="flex flex-col gap-5">
+            {!gameOver && (
+              <p className="mt-8 text-2xl font-bold text-gray-800">
+                Time left: {timer} seconds
+              </p>
+            )}
+
+            {!gameOver && (
+              <button
+                onClick={() => {
+                  if (isTimerRunning) {
+                    stopTimer();
+                  } else {
+                    timerIntervalRef.current = setInterval(() => {
+                      setTimer((prev) => {
+                        if (prev < 1) {
+                          clearInterval(timerIntervalRef.current!);
+                          socket.emit("timeOver", { roomId: id });
+                          return 0;
+                        }
+                        return prev - 1;
+                      });
+                    }, 1000);
+                  }
+                  setIsTimerRunning(!isTimerRunning);
+                }}
+                className={`p-3 font-bold rounded transition duration-300 ${
+                  isTimerRunning
+                    ? "bg-red-500 text-white hover:bg-red-600"
+                    : "bg-green-500 text-white hover:bg-green-600"
+                }`}
+              >
+                {isTimerRunning ? "Pause Timer" : "Resume Timer"}
+              </button>
+            )}
+          </div>
+          {resultString && (
+            <p className="mt-8 text-2xl font-bold text-gray-800">
+              {resultString}
+            </p>
+          )}
+        </>
+      )}
+      {gameOver && (
         <button
           onClick={() => {
-            if (isTimerRunning) {
-              if (timerIntervalRef.current) {
-                clearInterval(timerIntervalRef.current);
-                timerIntervalRef.current = null;
-              }
-            } else {
-              timerIntervalRef.current = setInterval(() => {
-                setTimer((prev) => {
-                  if (prev < 1) {
-                    clearInterval(timerIntervalRef.current!);
-                    socket.emit("timeOver", { roomId: id });
-                    return 0;
-                  }
-                  return prev - 1;
-                });
-              }, 1000);
-            }
-            setIsTimerRunning(!isTimerRunning);
+            window.location.reload();
           }}
-          className={`p-3 font-bold rounded transition duration-300 ${
-            isTimerRunning
-              ? "bg-red-500 text-white hover:bg-red-600"
-              : "bg-green-500 text-white hover:bg-green-600"
-          }`}
+          className="p-3 bg-purple-500 text-white font-bold rounded mt-4 transition duration-300 hover:bg-purple-600"
         >
-          {isTimerRunning ? "Pause Timer" : "Resume Timer"}
+          Play Again
         </button>
-      </div>
-      {resultString && (
-        <p className="mt-8 text-2xl font-bold text-gray-800">{resultString}</p>
       )}
     </div>
   );
