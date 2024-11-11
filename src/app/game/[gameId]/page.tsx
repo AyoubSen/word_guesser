901dc1259fa0b38c33e2a1f47046c637c1843760
Ayoub Senhaji <ayoub.senhaji@mazars.fr>
2024-11-11 20:42:18 +0100

    few fixes regarding timer, its also same for all players now, and we can now see players before starting game, and game can only be started if more than 1 players are there

--- src/app/game/[gameId]/page.tsx
+++ src/app/game/[gameId]/page.tsx
@@ -9,6 +9,12 @@ interface Dictionary {
   [word: string]: boolean;
 }
 
+interface User {
+  userName: string;
+  score: number;
+  lives: number;
+}
+
 const dictionary = dictionaryJson as unknown as Dictionary;
 
 export default function Home({ params }: { params: { gameId: string } }) {
@@ -20,15 +26,17 @@ export default function Home({ params }: { params: { gameId: string } }) {
   const [randomString, setRandomString] = useState<string>("");
   const [resultString, setResultString] = useState<string>("");
   const [userCount, setUserCount] = useState<number>(0);
-  const [users, setUsers] = useState<string[]>([]);
+  const [users, setUsers] = useState<User[]>([]);
   const [timer, setTimer] = useState<number>(10);
   const [isTimerRunning, setIsTimerRunning] = useState<boolean>(true);
   const [gameOver, setGameOver] = useState<boolean>(false);
   const [gameStarted, setGameStarted] = useState<boolean>(false);
+  const [winner, setWinner] = useState<string | null>(null);
 
   const timerIntervalRef = useRef<NodeJS.Timeout | null>(null);
 
   const defaultTimer: number = 10;
+
   useEffect(() => {
     if (!id) return;
 
@@ -44,7 +52,7 @@ export default function Home({ params }: { params: { gameId: string } }) {
     });
 
     socket.on("randomString", (sharedString: string) => {
-      if (!gameOver) {
+      if (!gameOver && gameStarted) {
         setRandomString(sharedString);
         setTimer(defaultTimer);
 
@@ -65,7 +73,7 @@ export default function Home({ params }: { params: { gameId: string } }) {
       }
     });
 
-    socket.on("userList", (userList: string[]) => {
+    socket.on("userList", (userList: User[]) => {
       setUsers(userList);
     });
 
@@ -81,6 +89,45 @@ export default function Home({ params }: { params: { gameId: string } }) {
       setResultString(message);
       stopTimer();
       setRandomString("");
+
+      if (message.startsWith("Game Over. Winner:")) {
+        const winnerName = message.split(": ")[1];
+        setWinner(winnerName);
+      }
+    });
+
+    socket.on("gameStarted", () => {
+      setGameStarted(true);
+      setResultString("The game has started!");
+    });
+
+    socket.on("gameError", (message: string) => {
+      setResultString(message);
+    });
+
+    socket.on("pauseTimerForAll", () => {
+      if (timerIntervalRef.current) {
+        clearInterval(timerIntervalRef.current);
+        timerIntervalRef.current = null;
+        setIsTimerRunning(false);
+      }
+    });
+
+    socket.on("resumeTimerForAll", () => {
+      if (!timerIntervalRef.current) {
+        timerIntervalRef.current = setInterval(() => {
+          setTimer((prev) => {
+            if (prev < 1) {
+              clearInterval(timerIntervalRef.current!);
+              timerIntervalRef.current = null;
+              socket.emit("timeOver", { roomId: id });
+              return 0;
+            }
+            return prev - 1;
+          });
+        }, 1000);
+        setIsTimerRunning(true);
+      }
     });
 
     return () => {
@@ -89,10 +136,9 @@ export default function Home({ params }: { params: { gameId: string } }) {
       }
       socket.disconnect();
     };
-  }, [id, userName, gameOver]);
+  }, [id, userName, gameOver, gameStarted]);
 
   const startGame = () => {
-    setGameStarted(true);
     socket.emit("startGame", { roomId: id });
   };
 
@@ -134,21 +180,48 @@ export default function Home({ params }: { params: { gameId: string } }) {
   return (
     <div className="flex flex-col justify-center items-center h-screen bg-[#DFF2EB] font-sans">
       <h1 className="text-4xl mb-8 text-gray-800 font-bold">Word Game</h1>
+      {!gameStarted && (
+        <>
+          <div className="text-lg mb-4 font-bold flex flex-col items-center">
+            Connected users:
+            <ul className="list-disc ml-5 font-normal">
+              {users.map((user, index) => (
+                <li key={index}>{user.userName}</li>
+              ))}
+            </ul>
+          </div>
+        </>
+      )}
+      {gameOver && winner && (
+        <p className="mt-8 text-2xl font-bold text-green-600">
+          Winner: {winner}
+        </p>
+      )}
       {gameOver && (
         <p className="mt-8 text-2xl font-bold text-red-600">Game Over</p>
       )}
       {!gameStarted ? (
         <button
           onClick={startGame}
-          className="p-3 bg-blue-500 text-white font-bold rounded mt-4 transition duration-300 hover:bg-blue-600"
+          className={`p-3 bg-blue-500 text-white font-bold rounded mt-4 transition duration-300 hover:bg-blue-600 ${
+            userCount > 1 ? "" : "cursor-not-allowed opacity-50"
+          }`}
+          disabled={userCount <= 1}
         >
           Start Game
         </button>
       ) : (
         <>
           <div>User count: {userCount}</div>
-          <div className="text-lg mb-4">
-            Connected users: {users.join(", ")}
+          <div className="text-lg mb-4 text-center font-bold">
+            Connected users:
+            <ul className="list-disc ml-5">
+              {users.map((user, index) => (
+                <li key={index}>
+                  {user.userName} - Score: {user.score} - Lives: {user.lives}
+                </li>
+              ))}
+            </ul>
           </div>
           {!gameOver && (
             <div className="text-5xl font-bold text-blue-500 mb-8 pb-5 p-3 rounded-lg bg-white shadow-lg">
@@ -178,17 +251,6 @@ export default function Home({ params }: { params: { gameId: string } }) {
                 >
                   Submit
                 </button>
-                {/* <button
-                  type="button"
-                  className="p-3 bg-red-500 text-white font-bold rounded transition duration-300 hover:bg-red-600"
-                  onClick={() => {
-                    setResultString("");
-                    setCurrentWord("");
-                  }}
-                  disabled={gameOver}
-                >
-                  Reset
-                </button> */}
               </div>
             </form>
           )}
@@ -198,12 +260,12 @@ export default function Home({ params }: { params: { gameId: string } }) {
                 Time left: {timer} seconds
               </p>
             )}
-
             {!gameOver && (
               <button
                 onClick={() => {
                   if (isTimerRunning) {
                     stopTimer();
+                    socket.emit("pauseTimer", { roomId: id });
                   } else {
                     timerIntervalRef.current = setInterval(() => {
                       setTimer((prev) => {
@@ -215,8 +277,9 @@ export default function Home({ params }: { params: { gameId: string } }) {
                         return prev - 1;
                       });
                     }, 1000);
+                    setIsTimerRunning(true);
+                    socket.emit("resumeTimer", { roomId: id });
                   }
-                  setIsTimerRunning(!isTimerRunning);
                 }}
                 className={`p-3 font-bold rounded transition duration-300 ${
                   isTimerRunning
